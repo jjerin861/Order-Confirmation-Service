6bc889540f304cd943bcf8d2eeac6da0faf1f952
jjerin861 <48552532+jjerin861@users.noreply.github.com>
2022-01-19 11:51:01 +0530

    Update order.go

--- internal/model/order.go
+++ internal/model/order.go
@@ -4,28 +4,28 @@ import "time"
 
 // Location model to hold latitude an longitude.
 type PaymentConfirmation struct {
-	OrderID        string `json:"order_id,omitempty"`
-	Amount         int    `json:"amount,omitempty"`
-	Payment_status string `json:"payment_status,omitempty"`
+	OrderID        string `json:"order_id"`
+	Amount         int    `json:"amount"`
+	Payment_status string `json:"payment_status"`
 }
 
 type OrderStatus struct {
-	OrderID                      string
-	Status                       string   `json:"status,omitempty"`
-	Amount                       int      `json:"amount,omitempty"`
-	ConfirmationsFailed          []string `json:"confirmations_failed,omitempty"`
-	ProcessingTimeMS             string   `json:"processing_time_ms,omitempty"`
-	PaymentConfirmationProcessed bool
-	FraudCheckProcessed          bool
-	VendorConfirmationProcessed  bool
-	StartTime                    time.Time
+	OrderID                      string    `json:"-"`
+	Status                       string    `json:"status"`
+	Amount                       int       `json:"amount"`
+	ConfirmationsFailed          []string  `json:"confirmations_failed"`
+	ProcessingTimeMS             string    `json:"processing_time_ms"`
+	PaymentConfirmationProcessed bool      `json:"-"`
+	FraudCheckProcessed          bool      `json:"-"`
+	VendorConfirmationProcessed  bool      `json:"-"`
+	StartTime                    time.Time `json:"-"`
 }
 
 type FraudCheck struct {
-	ReferenceID string `json:"reference_id,omitempty"`
-	RiskPoints  int    `json:"risk_points,omitempty"`
+	ReferenceID string `json:"reference_id"`
+	RiskPoints  int    `json:"risk_points"`
 }
 type VendorConfirmation struct {
-	OrderID string `json:"order,omitempty"`
-	Status  string `json:"status,omitempty"`
+	OrderID string `json:"order"`
+	Status  string `json:"status"`
 }
